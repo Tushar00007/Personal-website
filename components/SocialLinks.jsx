c0cac2a7a5a54967e68dae907a93016969afdc2b
Rahulk612 <rahulkonda612@gmail.com>
2022-02-12 13:07:39 +0530

    final

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -13,56 +13,33 @@ const SocialLinks = () => {
         href={socialLinks.email}
         target="_blank"
         rel="noopener"
-        aria-label="Twitter"
-      >
-        <span className="btn-inner--icon">
-          <i className="fa fa-twitter" />
-        </span>
-      </Button>
-      <Button
-        className="btn-icon-only rounded-circle ml-1"
-        color="twitter"
-        rel="noopener"
         aria-label="email"
-        href={socialLinks.twitter}
-        target="_blank"
       >
         <span className="btn-inner--icon">
           <i className="fa fa-envelope" />
         </span>
       </Button>
+
       <Button
         className="btn-icon-only rounded-circle ml-1"
-        color="facebook"
+        color="linkedin"
         href={socialLinks.linkedin}
         target="_blank"
         rel="noopener"
-        aria-label="Facebook"
+        aria-label="Linkedin"
       >
         <span className="btn-inner--icon">
-          <i className="fa fa-facebook-square" />
+          <i className="fa fa-linkedin" />
         </span>
       </Button>
 
       <Button
         className="btn-icon-only rounded-circle ml-1"
-        color="instagram"
+        color="github"
         href={socialLinks.github}
         target="_blank"
         rel="noopener"
-        aria-label="Instagram"
-      >
-        <span className="btn-inner--icon">
-          <i className="fa fa-instagram" />
-        </span>
-      </Button>
-      <Button
-        className="btn-icon-only rounded-circle ml-1"
-        color="github"
-        href={socialLinks.instagram}
-        rel="noopener"
-        aria-label="Github"
-        target="_blank"
+        aria-label="github"
       >
         <span className="btn-inner--icon">
           <i className="fa fa-github" />
@@ -72,12 +49,36 @@ const SocialLinks = () => {
         className="btn-icon-only rounded-circle ml-1"
         color="twitter"
         rel="noopener"
-        aria-label="Linkedin"
+        aria-label="twitter"
+        href={socialLinks.twitter}
+        target="_blank"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-twitter" />
+        </span>
+      </Button>
+      <Button
+        className="btn-icon-only rounded-circle ml-1"
+        color="instagram"
+        href={socialLinks.instagram}
+        rel="noopener"
+        aria-label="instagram"
+        target="_blank"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-instagram" />
+        </span>
+      </Button>
+      <Button
+        className="btn-icon-only rounded-circle ml-1"
+        color="facebook"
+        rel="noopener"
+        aria-label="facebook"
         href={socialLinks.facebook}
         target="_blank"
       >
         <span className="btn-inner--icon">
-          <i className="fa fa-linkedin" />
+          <i className="fa fa-facebook-square" />
         </span>
       </Button>
     </div>
